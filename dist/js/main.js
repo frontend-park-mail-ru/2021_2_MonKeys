8ba0cee5e6177200c411999fdb6fb5bcfac77cdd
Ilyagu <inagdimaev@mail.ru>
2021-09-22 16:42:07 +0300

    profile page done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,88 +1,185 @@
 'use strict';
 
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-document.addEventListener('touchend', handleTouchEnd, false);
+// document.addEventListener('touchstart', handleTouchStart, false);
+// document.addEventListener('touchmove', handleTouchMove, false);
+// document.addEventListener('touchend', handleTouchEnd, false);
 
-let body = document.querySelector('body');
-let counter = 0;
+// let body = document.querySelector('body');
+// let counter = 0;
 let sample = [{
-        'firstName': 'Elon',
-        'age': "25",
-        'photoSrc': "dist/img/Elon_Musk_2015.jpg",
-        'colorFrom': 'grey',
-        'colorTo': 'black',
+        'name': 'Elon',
+        'age': '25',
+        'photoSrc': 'dist\\img\\Elon_Musk_2015.jpg',
+        'text': `Инст: @elonmask<br>
+        Всем привет меня зовут Илон Маск. Люблю играть в
+        геншин импакт, обажаю гаремники. 
+        Просто не могу жить без иссекаев,
+        мое любимое аниме это конечно же Ван Пис ахахах)
+        Я посмотрел все 1000+ серий
+        так же у меня есть краш, поэтому ищу ТОЛЬКО ДРУГА
+        в общем буду твоим сенпаем, писать в инсту, 
+        а то drip лагает ахахахах)`,
+        'tags' : [
+            'anime',
+            'IT',
+            'music',
+            'soccer',
+        ]
     },
     {
-        'firstName': 'Ilyagu',
-        'age': "20",
-        'photoSrc': "dist/img/kUoqFCTbj0Y.jpg",
-        'colorFrom': 'white',
-        'colorTo': 'red',
+        'name': 'Michail',
+        'age': '20',
+        'photoSrc': 'dist\\img\\Elon_Musk_2015.jpg',
+        'text': `
+        Инст: @marvin<br>
+        Тг: @Marvin<br>
+        Првиет я Миша мне 2 года
+        люблю сосать члены, господи как же я люблю хуи
+        по жизни пассив конченный
+        обоссанцец. Обожаю играть в геншин
+        `,
+        'tags' : [
+            'banana',
+            'fullstack',
+            'gay',
+            'anal',
+        ]
     },
-    {
-        'firstName': 'Ilyagu2',
-        'age': "21",
-        'photoSrc': "dist/img/aj1-V9h9GGM.jpg",
-        'colorFrom': 'pink',
-        'colorTo': 'red',
-    },
-    {
-        'firstName': 'Ilyagu3',
-        'age': "22",
-        'photoSrc': "dist/img/kUoqFCTbj0Y.jpg",
-        'colorFrom': 'white',
-        'colorTo': 'red',
-    }
 ]
 
+function fillCardMain() {
 
+    const cardMain = document.getElementById('cardMainID')
 
-function nextCharacter() {
-    const currentobj = sample[counter];
-    counter++;
-    let elonInside = `
-    <img src="${currentobj.photoSrc}" class="profile-image">
+    const lol = sample[0];
+    
+    const img = document.createElement('img');
+    img.src = `${lol.photoSrc}`;
+    img.className = 'card-el profile-image';
+    cardMain.appendChild(img);
+    
+    const divName = document.createElement('div');
+    divName.className = 'name';
+    divName.textContent = `${lol.name}, ${lol.age}`;
+    cardMain.appendChild(divName);
+    
+    const divBord = document.createElement('div');
+    divBord.className = 'card-el bord';
+    cardMain.appendChild(divBord);
+    
+    const divDesc = document.createElement('div');
+    divDesc.className = 'card-el desc';
+    divDesc.innerHTML = `${lol.text}`;
+    cardMain.appendChild(divDesc);
+    
+    const divBord2 = document.createElement('div');
+    divBord2.className = 'card-el bord';
+    cardMain.appendChild(divBord2);
+    
+    const divTags = document.createElement('div');
+    divTags.id = 'tagsID'
+    divTags.className = 'card-el tags-container';
+    cardMain.appendChild(divTags);
 
-            <div class="bottom-panel">
-                <div class="name">
-                    ${currentobj.firstName}
-                    ${currentobj.age}
-                </div>
-                <div class="class actions-container">
-                    <button class="menu-icon">
-                        <img src="dist/icons/button_dislike_white.svg" width="50px" height="50px" alt="drop">           
-                    </button>
-                    <button class="menu-icon">
-                        <img src="dist/icons/button_expand_white.svg" width="50px" height="50px" alt="drop">           
-                    </button>
-                    <button class="menu-icon">
-                        <img src="dist/icons/tapbar_likes_white_selected.svg" width="50px" height="50px" alt="drop">           
-                    </button>
-                </div>
-            </div>
-    `;
-    body.style.background = `linear-gradient(120deg, ${currentobj.colorFrom}, ${currentobj.colorTo})`;
-    let card1new = document.getElementsByClassName("card2")[0]
-    card1new.className = "card"
-    card1new.innerHTML = elonInside;
-    let card3new = document.getElementsByClassName("card3")[1]
-    card3new.className = "card2"
-    let cardOld = document.getElementsByClassName("card")[1];
-    body.removeChild(cardOld)
-    let nextCard = document.createElement('div');
-    nextCard.className = 'card3'
-    let temp = body.innerHTML
-    let newBody = '<div class="card3"></div>' + temp;
-    body.innerHTML = newBody;
-    currentCard = document.getElementsByClassName("card")[0]
-
-    // previousCard = document.getElementsByClassName("card2")[0]
-    // previousCard2 = document.getElementsByClassName("card3")[1]
-
-    x1 = null;
-    y1 = null;
-
-    x = null;
-    y = null;
+    for (var tag in lol.tags) {
+        const buttonTag = document.createElement('button');
+        buttonTag.className = 'tag';
+        buttonTag.innerHTML = `${lol.tags[tag]}`;
+        divTags.appendChild(buttonTag)
+      }
 }
+
+function profileRender() {
+    const divMain = document.getElementById('main');
+
+    const divCrad = document.createElement('div');
+    divCrad.id = 'cardID';
+    divCrad.className = 'card';
+    divMain.appendChild(divCrad);
+
+    const divTapBar = document.createElement('div');
+    divTapBar.id = 'tapbarID';
+    divTapBar.className = 'tapbar-container';
+    divMain.appendChild(divTapBar);
+
+    fillCard();
+    fillTapBar();
+}
+
+function fillCard() {
+    const divCrad = document.getElementById('cardID')
+
+    const divCardMain = document.createElement('div');
+    divCardMain.id = 'cardMainID';
+    divCardMain.className = 'card-main';
+    divCrad.appendChild(divCardMain);
+
+    fillCardMain();
+
+    const divEdit = document.createElement('div');
+    divEdit.id = 'editID';
+    divEdit.className = 'forEdit';
+    divCrad.appendChild(divEdit);
+
+    fillEdit();
+}
+
+function fillEdit() {
+    const divEdit = document.getElementById('editID');
+
+    const buttonEdit = document.createElement('button');
+    buttonEdit.className = 'menu-icon';
+    divEdit.appendChild(buttonEdit)
+    const imgEdit = document.createElement('img');
+    imgEdit.src = 'dist/icons/button_edit_white.svg';
+    imgEdit.style.width = '50px';
+    imgEdit.style.height = '50px';
+    imgEdit.alt = 'edit';
+    buttonEdit.appendChild(imgEdit);
+}
+
+function fillTapBar() {
+    const divTapBar = document.getElementById('tapbarID');
+
+    const buttonDropDesel = document.createElement('button');
+    buttonDropDesel .className = 'menu-icon';
+    divTapBar.appendChild(buttonDropDesel)
+    const imgDropDesel = document.createElement('img');
+    imgDropDesel.src = 'dist/icons/tapbar_feed_white_deselected.svg';
+    imgDropDesel.style.width = '50px';
+    imgDropDesel.style.height = '50px';
+    imgDropDesel.alt = 'drop';
+    buttonDropDesel.appendChild(imgDropDesel);
+
+    const buttonLikeDesel = document.createElement('button');
+    buttonLikeDesel .className = 'menu-icon';
+    divTapBar.appendChild(buttonLikeDesel)
+    const imgLikeDesel = document.createElement('img');
+    imgLikeDesel.src = 'dist/icons/tapbar_likes_white_deselected.svg';
+    imgLikeDesel.style.width = '50px';
+    imgLikeDesel.style.height = '50px';
+    imgLikeDesel.alt = 'like';
+    buttonLikeDesel.appendChild(imgLikeDesel);
+
+    const buttonMessageDesel = document.createElement('button');
+    buttonMessageDesel .className = 'menu-icon';
+    divTapBar.appendChild(buttonMessageDesel)
+    const imgMessageDesel = document.createElement('img');
+    imgMessageDesel.src = 'dist/icons/tapbar_chat_white_deselected.svg';
+    imgMessageDesel.style.width = '50px';
+    imgMessageDesel.style.height = '50px';
+    imgMessageDesel.alt = 'drop';
+    buttonMessageDesel.appendChild(imgMessageDesel);
+
+    const buttonProfileSelec = document.createElement('button');
+    buttonProfileSelec .className = 'menu-icon';
+    divTapBar.appendChild(buttonProfileSelec)
+    const imgProfileSelec = document.createElement('img');
+    imgProfileSelec.src = 'dist/icons/tapbar_user_white_selected.svg';
+    imgProfileSelec.style.width = '50px';
+    imgProfileSelec.style.height = '50px';
+    imgProfileSelec.alt = 'drop';
+    buttonProfileSelec.appendChild(imgProfileSelec); 
+}
+
+profileRender();
