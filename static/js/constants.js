c73983c85de494a89c6ffb919573eb4563cb74d1
Perlin Leonid <perlinleo@gmail.com>
2021-10-02 02:20:40 +0300

    circleCI3try

--- static/js/constants.js
+++ static/js/constants.js
@@ -3,6 +3,5 @@ const serverAddress = 'https://api.ijia.me';
 const profileURL = '/api/v1/profile';
 const authURL = '/api/v1/auth';
 const feedURL = '/api/v1/feed';
-
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
