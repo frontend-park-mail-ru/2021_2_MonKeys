73a3a6b7c7e3b9089996c5bef821fcfcfe0c006e
Perlin Leonid <perlinleo@gmail.com>
2021-10-02 02:17:24 +0300

    circleCI2

--- static/js/constants.js
+++ static/js/constants.js
@@ -5,5 +5,4 @@ const authURL = '/api/v1/auth';
 const feedURL = '/api/v1/feed';
 
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
 const passwordRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
