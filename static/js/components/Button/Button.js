51bdee3879bcfcae3a3ec93516d956191ef1e2ce
Perlin Leonid <perlinleo@gmail.com>
2021-10-14 00:56:44 +0300

    flux part 1

---
+++ static/js/components/Button/Button.js
@@ -0,0 +1,5 @@
+/**
+ * 
+ * PARAMETRI---->>>> HTML COD KNOPKI
+ *
+ */
---
+++ static/js/requests/sessionRequest.js
@@ -0,0 +1,41 @@
+import RequestBase from "./requestBase";
+import { serverAddress, sessionURL } from "../constants/urls";
+
+/**
+ * login
+ * logout
+ */
+export default class SessionRequest {
+    _email
+    _password
+    _requestLogin
+    _requestLogout
+
+    constructor(email, password) {
+        this._requestLogin = new RequestBase({
+            'email': email,
+            'password': password,
+        }, {
+            'Content-Type': 'application/json',
+        }, {
+            'Method': 'POST',
+        })
+
+        this._requestLogout = new RequestBase({}, {
+            'Content-Type': 'application/json',
+        }, {
+            'Method': 'DELETE',
+        })
+    }
+
+    login(resolve) {
+        this._requestLogin.send(`${serverAddress}${sessionURL}`,
+            resolve
+        )
+    }
+    logout(resolve) {
+        this._requestLogout.send(`${serverAddress}${sessionURL}`,
+            resolve
+        )
+    }
+}
