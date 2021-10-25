b55b41a9060aa905c2ef0ec42eed8f13cdfa33ef
perlinleo <perlinleo@github.com>
2021-10-25 20:00:36 +0300

    router fix

--- source/route/router.ts
+++ source/route/router.ts
@@ -11,20 +11,26 @@ class Router {
     go(route: string) {
         if (route) {
             window.history.pushState({},'',route);
+            
         }
         const $root = document.getElementById('app');
         const location = route;
         if(this.routes[location]){
-
             if(this.routes[location].auth && AuthStore.get().loggedIn){
                 const currentView = new this.routes[location].view($root);
+                window.document.title = this.routes[location].name;
                 currentView.render()
-            } else {
+            } else if (this.routes[location].auth && !AuthStore.get().loggedIn){
                 const currentView = new this.routes['/login'].view($root);
+                window.document.title = this.routes[location].name;
                 window.history.pushState({},'','/login');
                 currentView.render()
+            } else {
+                const currentView = new this.routes[route].view($root);
+                window.document.title = this.routes[location].name;
+                window.history.pushState({},'', route);
+                currentView.render()
             }
-
         } else {
             // console.log(this.routes['/404'])
             window.location.pathname = '/404'; 
