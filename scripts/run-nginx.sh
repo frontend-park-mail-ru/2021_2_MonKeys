aec851c90b104fb9eb7e60ea67accb92479046a3
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-10-26 02:44:57 +0300

    fix  run-nginx.sh

--- scripts/run-nginx.sh
+++ scripts/run-nginx.sh
@@ -2,6 +2,14 @@
 
 cd ..
 sudo service nginx stop
-sudo cp Drip.nginx /etc/nginx/nginx.conf
+
+CONFIG_NAME="Drip.nginx"
+STATIC_PATH=$(grep "/static/" $CONFIG_NAME)
+NEW_STATIC_PATH=" \t\t\troot ""${PWD}""/static/;"
+
+sed -i "s|$STATIC_PATH|$NEW_STATIC_PATH|g" $CONFIG_NAME
+
+sudo cp $CONFIG_NAME /etc/nginx/nginx.conf
+
 sudo service nginx start
 sudo nginx -t
