db6f84e7caa710c02500b4d0c32cc76201e30b00
perlinleo <perlinleo@github.com>
2021-10-31 17:59:41 +0300

    ddd

--- source/store/profileStore.ts
+++ source/store/profileStore.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import BaseStore from './storeBase.js';
 
 export interface ProfileData {
@@ -18,20 +17,3 @@ ProfileStore.set({
 });
 
 export { ProfileStore };
-=======
-import BaseStore from './storeBase.js';
-
-export interface ProfileData {
-    id?: string;
-    name?: string;
-    age?: string;
-    date?: string;
-    description?: string;
-    imgSrc?: string[];
-    tags?: Set<string>;
-}
-
-const ProfileStore = new BaseStore<ProfileData>();
-
-export { ProfileStore };
->>>>>>> b29b41b2a504e6815c84e537aa02dfe8b109a80f
