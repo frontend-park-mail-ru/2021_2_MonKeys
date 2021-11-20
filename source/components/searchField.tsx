bb99a72e2a84f155a876de4ebbee40a0a66cb595
4Marvin2 <mumeu222@mail.ru>
2021-11-20 19:05:04 +0300

    fixed linters

--- source/components/searchField.tsx
+++ source/components/searchField.tsx
@@ -3,16 +3,14 @@ import eventBus from '../dispatcher/eventBus.js';
 import reactions from '../constants/reactions.js';
 import ViewBase from '../views/viewBase.js';
 
-export interface SearchFieldProps {}
-
 export const SearchField = () => {
     const searchEvent = () => {
         eventBus.dispatch('chat:search');
     };
     return (
         <form class='search-form'>
-            <input oninput={searchEvent} type='text' placeholder='Поиск' class='search-form__input'/>
-            <img src='icons2/search.svg' class='search-form__icon'/>
+            <input oninput={searchEvent} type='text' placeholder='Поиск' class='search-form__input' />
+            <img src='icons2/search.svg' class='search-form__icon' />
         </form>
     );
 };
