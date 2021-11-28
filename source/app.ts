ce9553cb726729cf87f7129426ab518e67e98a1a
perlinleo <perlinleo@github.com>
2021-11-29 01:51:15 +0300

    throttle.ts / client.ts

--- source/app.ts
+++ source/app.ts
@@ -3,8 +3,10 @@ import router from './route/router.js';
 import { InitBus } from './dispatcher/events.js';
 import ViewBase from './views/viewBase.js';
 import { resetDefaults } from './modules/resets.js';
+import { isWidescreen } from './utils/client.js';
 
 resetDefaults(document.getElementById('app'));
+window.addEventListener('resize', isWidescreen);
 
 InitBus();
 
