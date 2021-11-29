1fcce8a9e89898abd629fba836d0a3ba5826d4a8
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-11-29 03:10:13 +0300

    profile flex

--- source/components/profileActions.tsx
+++ source/components/profileActions.tsx
@@ -2,5 +2,9 @@ import { MonkeysVirtualDOM } from '../virtualDOM/virtualDOM.js';
 import { IconButton } from './iconButton.js';
 
 export const ProfileActions = (props) => {
-    return <div class='profile-actions'>{Object.keys(props).map((item) => IconButton(props[item]))}</div>;
+    return (
+      <div class='view-content__profile-actions'>
+          <div class='profile-actions'>{Object.keys(props).map((item) => IconButton(props[item]))}</div>
+      </div>
+    );
 };
