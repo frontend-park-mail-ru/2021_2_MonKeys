94883d92fd1096d070965848af9ba38271fb69ec
perlinleo <perlinleo@github.com>
2021-12-14 21:11:51 +0300

    unreg sw

--- source/app.ts
+++ source/app.ts
@@ -3,7 +3,7 @@ import router from './route/router.js';
 import { InitBus } from './dispatcher/events.js';
 import ViewBase from './views/viewBase.js';
 import { isWidescreen, startClientAspectRatio } from './utils/client.js';
-import { registerServiceWorker } from './service/serviceWorkerRegister.js';
+// import { registerServiceWorker } from './service/serviceWorkerRegister.js';
 import { EVENTS } from './dispatcher/events.js';
 import '../scss/main.scss';
 
@@ -26,11 +26,12 @@ window.onpopstate = () => {
 EventBus.dispatch<string>(EVENTS.USER_COOKIE_REQUESTS);
 window.history.pushState('', '', window.location.pathname);
 
-registerServiceWorker();
+// registerServiceWorker();
 
 const percent = 0.01;
 const vh = window.innerHeight * percent;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 window.addEventListener('resize', () => {
     const vh = window.innerHeight * percent;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
