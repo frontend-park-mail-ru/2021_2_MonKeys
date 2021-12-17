cf7e6cf7d9a205dbacf2bae19aa9a7678829d609
perlinleo <perlinleo@github.com>
2021-12-17 15:04:44 +0300

    Google SIte verification

--- scripts/build.py
+++ scripts/build.py
@@ -36,6 +36,7 @@ if __name__ == "__main__":
 <html lang="en">
 
     <head>
+        <meta name="google-site-verification" content="svHOuSQfBAcUYt7xCVKGRkTxwLrHgAzxCFBOHugby8w" />
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
